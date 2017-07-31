cc39e2952667b7e4097470afdceab7b9621a3c96
hpn33 <hamed.hpn332@gmail.com>
2017-07-31 12:14:25 +0430

    base

---
+++ app/src/main/java/hpn332/cb/AddTask/AddTaskActivity.java
@@ -0,0 +1,15 @@
+package hpn332.cb.AddTask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hpn332.cb.R;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+    }
+}
